85aac9bb48d9a977564514f466c24b8b76182ff8
shamshersarpal <shamshersingh01567@gmail.com>
2025-01-31 18:30:37 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -28,8 +28,8 @@ export default function Home() {
               type="email"
               id="email"
               className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-100 focus:border-primary-600 block w-full p-2.5"
-              placeholder="enter your email"
-              name="email"
+              placeholder=" " value="test@gmail.com"
+              name="email" readOnly 
             />
           </div>
           <div>
@@ -42,7 +42,8 @@ export default function Home() {
             <input
               type="password"
               id="password"
-              placeholder="••••••••"
+              placeholder=""
+              value="••••••••" readOnly
               className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
               name="password"
             />
